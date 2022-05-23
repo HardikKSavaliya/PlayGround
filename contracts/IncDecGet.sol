c54291867760b582ff1051368a1b04013cdfbf2b
Hardik Savaliya <hardik_savaliya@hotmail.com>
2022-05-23 09:17:09 +0530

    added an web3 connection with contract

---
+++ contracts/IncDecGet.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract IncDecGet {
+    uint256 number;
+
+    constructor() {
+        number = 0;
+    }
+
+    function increment() public {
+        number = number + 1;
+    }
+
+    function decrement() public {
+        number = number + 1;
+    }
+
+    function get() public view returns (uint256) {
+        return number;
+    }
+}
