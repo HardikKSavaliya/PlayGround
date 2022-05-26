02c6c9b7e48ef004057ab4e57b381558e6281a21
Hardik Savaliya <hardik_savaliya@hotmail.com>
2022-05-26 10:07:59 +0530

    added an truffle configurations

--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -34,9 +34,9 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
-  dashboard: {
-    port: 24012,
-  },
+  // dashboard: {
+  //   port: 24012,
+  // },
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
@@ -62,14 +62,14 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // ropsten: {
-    //   provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    //   network_id: 3,       // Ropsten's id
-    //   gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/<Project ID>`),
+      network_id: 4,      
+      gas: 5500000,       
+      confirmations: 2,   
+      timeoutBlocks: 200,  
+      skipDryRun: true
+    },
     //
     // Useful for private networks
     // private: {
