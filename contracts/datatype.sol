028546b5a61eca436f108f478666e64c5f402d33
Hardik Savaliya <hardik_savaliya@hotmail.com>
2022-06-01 09:06:08 +0530

    data type

--- contracts/datatype.sol
+++ contracts/datatype.sol
@@ -2,6 +2,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract datatype {
-  constructor() public {
-  }
+    string public str = "Solidity state variable";
+
+    function printInt() public pure returns (uint256) {
+        uint256 number = 7;
+        return number;
+    }
+
+    function printString() public pure returns (string memory) {
+        string memory name = "Eth";
+        return name;
+    }
 }
