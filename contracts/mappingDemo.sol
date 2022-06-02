c6f34f15317c062c69b9dc808a4f36526f2365d8
Hardik Savaliya <hardik_savaliya@hotmail.com>
2022-06-02 09:27:09 +0530

    mapping structure

--- contracts/mappingDemo.sol
+++ contracts/mappingDemo.sol
@@ -3,8 +3,21 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract mappingDemo {
     mapping(uint8 => string) public student;
+    struct studentDetails {
+        string name;
+        uint8 age;
+    }
+    mapping(uint256 => studentDetails) public studentDetail;
 
     function input(uint8 _rollNo, string memory _name) public {
         student[_rollNo] = _name;
+    }
+
+    function inputStruct(
+        uint8 _rollNo,
+        string memory _name,
+        uint8 _age
+    ) public {
+        studentDetail[_rollNo] = studentDetails(_name, _age);
     }
 }
