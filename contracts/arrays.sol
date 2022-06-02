46279e5d65c08681a99d41da863b658850a1eeca
Hardik Savaliya <hardik_savaliya@hotmail.com>
2022-06-02 06:20:46 +0530

    dyanamic size array

--- contracts/arrays.sol
+++ contracts/arrays.sol
@@ -3,12 +3,25 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract arrays {
     uint8[4] public fixedSizeArray;
+    uint8[] public dynamicSizeArray;
 
-    function insert(uint8 position, uint8 value) public {
+    function insertFixedSize(uint8 position, uint8 value) public {
         fixedSizeArray[position] = value;
     }
 
-    function length() public view returns (uint256) {
+    function lengthFixedSize() public view returns (uint256) {
         return fixedSizeArray.length;
+    }
+
+    function insertDynamicSize(uint8 value) public {
+        dynamicSizeArray.push(value);
+    }
+
+    function removeDynamicSize() public {
+        dynamicSizeArray.pop();
+    }
+
+    function lengthDynamicSize() public view returns (uint256) {
+        return dynamicSizeArray.length;
     }
 }
